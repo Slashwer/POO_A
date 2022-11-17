ca97cd2339b0402b91977efa6519a324b8595445
Jostyn Fabricio Muentes Roca <74916685+Slashwer@users.noreply.github.com>
2022-11-17 12:25:48 -0500

    Add files via upload

---
+++ Ejercicio 2/Principal.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Numero Primer = new Numero();
+        Primer.NumeroPar();
+    }
+}
